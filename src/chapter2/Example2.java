7dfd195b7c6ec8acfbb6fd90a245dbbd0cd88143
gurgen melikyan <melikyan07@gmail.com>
2022-12-15 11:59:54 +0400

    add

---
+++ src/chapter2/Example2.java
@@ -0,0 +1,14 @@
+package chapter2;
+
+public class Example2 {
+    public static void main(String[] args) {
+        int num;
+        num = 100;
+        System.out.println("num " + num);
+        num = num * 2;
+        System.out.print("Знaчeниe переменной num * 2 равно ");
+        System.out.println(num);
+
+}
+    }
+
---
+++ src/chapter3/BoolTest.java
@@ -0,0 +1,18 @@
+package chapter3;
+
+public class BoolTest {
+    public static void main(String[] args) {
+        boolean b;
+        b = false;
+        System.out.println("b равно " + b);
+        b = true;
+        System.out.println("b = " + b);
+        if (b) {
+            System.out.println("Этoт код выполняется. ");
+
+
+        }
+        System.out.println("10>9 = " + (10 > 9));
+
+    }
+}
