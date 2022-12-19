5c2b6f481e70157d87cd708b6e05d6c92d4a0ff2
gurgen melikyan <melikyan07@gmail.com>
2022-12-19 16:43:48 +0400

    Example Conversion

--- src/chapter2/BlockTest.java
+++ src/chapter2/BlockTest.java
@@ -2,38 +2,12 @@ package chapter2;
 
 public class BlockTest {
     public static void main(String[] args) {
-//        int x, y;
-//        y = 20;
-//        for (x = 0; x < 10; x++) {
-//            System.out.println("x " + x);
-//            System.out.println("y " + y);
-//            y = y - 2;
-//        }
-//        for (int i = 0; i < 6; i++) {
-//            for (int j = 0; j < i; j++){
-//                System.out.print("*");
-//
-//
-//            }
-//            System.out.println();
-//        }
-//        for (int i = 5; i > 0; i--) {
-//            for (int j = i; j > 0; j--) {
-//                System.out.print("*");
-//
-//            }
-//            System.out.println();
-//        }
-        for (int i = 0; i <5 ; i++) {
-            for (int j = 5; j > i; j--) {
-                System.out.print("  ");
-
-            }
-            for (int j = 0; j <=i; j++) {
-                System.out.print("* ");
-
-            }
-            System.out.println();
+        int x, y;
+        y = 20;
+        for (x = 0; x < 10; x++) {
+            System.out.println("x " + x);
+            System.out.println("y " + y);
+            y = y - 2;
         }
     }
 
