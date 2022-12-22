1e0077cae28b0866f805ad9cb55a2f8fcb20bfdf
gurgen melikyan <melikyan07@gmail.com>
2022-12-22 21:23:43 +0400

    add

--- src/homework/FigurePainter.java
+++ src/homework/FigurePainter.java
@@ -14,15 +14,13 @@ public class FigurePainter {
             }
             for (int j = 0; j <= i; j++) {
                 System.out.print("* ");
-
             }
             System.out.println();
         }
+
         for (int i = 0; i < 5; i++) {
             for (int j = 4; j > i; j--) {
                 System.out.print("* ");
-
-
             }
             System.out.println();
         }
