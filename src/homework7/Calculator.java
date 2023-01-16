eeceec191f1e6dcd05132c23fc1e33db7a4e41da
gurgen melikyan <melikyan07@gmail.com>
2023-01-16 23:10:56 +0400

    add

--- src/homework7/Calculator.java
+++ src/homework7/Calculator.java
@@ -4,30 +4,30 @@ public class Calculator {
     double x;
     double y;
 
-    double plus() {
+    double plus(double x, double y) {
         return x + y;
     }
 
-    double minus() {
+    double minus(double x, double y) {
         return x - y;
     }
 
-    double multiply() {
+    double multiply(double x, double y) {
         return x * y;
     }
 
-    double divide() {
+    double divide(double x, double y) {
         if (y == 0) {
             System.out.println("թիվը 0 ի չի բաժանվում");
             return -1;
         }
-        return x / 2;
+        return x / y;
     }
 
-    void setDim(double x, double y) {
-        this.x = x;
-        this.y = y;
+//    void setDim(double x, double y) {
+//        this.x = x;
+//        this.y = y;
 
     }
-}
+
 
--- src/homework7/CalculatorDemo.java
+++ src/homework7/CalculatorDemo.java
@@ -4,15 +4,10 @@ public class CalculatorDemo {
     public static void main(String[] args) {
         Calculator xy = new Calculator();
 
-        xy.setDim(1, 2);
-        System.out.println(xy.plus());
-        xy.setDim(3, 4);
-        System.out.println(xy.minus());
-        xy.setDim(2, 6);
-        System.out.println(xy.multiply());
-        xy.setDim(7, 2);
-        System.out.println(xy.divide());
-        xy.setDim(4, 0);
-        System.out.println(xy.divide());
+        System.out.println(xy.plus(2,3));
+        System.out.println(xy.minus(4,4));
+        System.out.println(xy.multiply(7,6));
+        System.out.println(xy.divide(9,0));
+        System.out.println(xy.divide(39,65));
     }
 }
